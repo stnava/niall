92f6b8a6e9d8e489b9fd791c34ee3b2ac06f8a47
stnava <stnava@gmail.com>
2021-11-11 13:26:29 -0500

    ENH: dti recon at SR

--- src/dtirecon/02_job_script.py
+++ src/dtirecon/02_job_script.py
@@ -5,65 +5,92 @@ os.environ["TF_NUM_INTRAOP_THREADS"] = nth
 os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = nth
 from os.path import exists
 import glob
+import re
 rootdir = "/mnt/cluster/data/PPMI2/PPMI/"
 if not exists( rootdir ):
     rootdir = "/Users/stnava/data/PPMI2/"
     print("rootdir " + rootdir )
 modality = "DTI_LR"
+modality2 = "DTI_RL"
 targetfns = glob.glob( rootdir + "*/*/*/*/dcm2niix/V0/*" +
-    modality + "*dcm2niix-V0.nii.gz" )
+    modality + "*dcm2niix-V0-SR.nii.gz" )
 if len(targetfns) == 0:
     targetfns = glob.glob( rootdir + "*/*/*/*/*/dcm2niix/V0/*" +
-    modality + "*dcm2niix-V0.nii.gz" )
+    modality + "*dcm2niix-V0-SR.nii.gz" )
 import sys
 fileindex = 0
 if len( sys.argv ) > 1:
     fileindex = int(sys.argv[1])
 targetfn = targetfns[ fileindex ]
-import re
+targetfn2 = re.sub(modality, modality2, targetfn )
+targetsplit = targetfn.split("/")
+targetfn2 = glob.glob( re.sub( targetsplit[9], "*", targetfn2 ))[0]
+if not exists( targetfn ) or not exists( targetfn2 ) :
+    print( targetfn2 + "does not exist")
+    sys.exit(1)
+
+
 # FIXME - more specific stuff below for this modality
-mysubbed = re.sub('T1w', 'T1wHierarchical', targetfn )
-mysubbedsplit = mysubbed.split("/")
-# define the directories and create them
-newoutdir = ''
-newprefix = ''
-keyindex = 10 # change for each case
-for k in range(keyindex):
-    newoutdir = newoutdir + '/' + mysubbedsplit[k]
-    if k > 5:
-        newprefix = newprefix + mysubbedsplit[k] + '-'
-newprefix = newoutdir + '/' + newprefix
-# create the directory
-myx = os.path.isdir( newoutdir )
-print( "make " +  newoutdir + " " + str( myx ) )
+mysubbed = re.sub( modality, 'DTIJoinL', targetfn )
+mysubbed2 = re.sub( modality2, 'DTIJoinR', targetfn2 )
+newprefixList = []
+newoutdirList = []
+for newout in [mysubbed, mysubbed2]:
+    mysubbedsplit = newout.split("/")
+    # define the directories and create them
+    newoutdir = ''
+    newprefix = ''
+    keyindex = 10 # change for each case
+    for k in range(keyindex):
+        newoutdir = newoutdir + '/' + mysubbedsplit[k]
+        if k > 5:
+            newprefix = newprefix + mysubbedsplit[k] + '-'
+    newprefix = newoutdir + '/' + newprefix
+    # create the directory
+    myx = os.path.isdir( newoutdir )
+    print( "make " +  newoutdir + " " + str( myx ) )
+    if not myx:
+        os.makedirs( newoutdir )
+    print( "made " +  newoutdir + " successfully " )
+    newprefixList.append( newprefix )
+    newoutdirList.append( newoutdir )
 
-if not myx:
-    os.makedirs( newoutdir )
-
-print( "made " +  newoutdir + " successfully " )
-outfn = newprefix + "hippR" + '.nii.gz'
-myoutfnexists = exists( outfn )
-if myoutfnexists:
-    print( outfn + "exists already" )
+myoutfnexists = exists( mysubbed )
+myoutfnexists2 = exists( newprefixList[1] + 'SRRGB.nii.gz' )
+if myoutfnexists2:
+    print( newprefixList[1] + 'SRRGB.nii.gz' + "exists already" )
     sys.exit()
 
-print( "continue " +  outfn + " run " )
+print( "continue " +  mysubbed + " run " )
+
 import ants
 import antspymm
 import tensorflow as tf
 import antspyt1w
-targetImage = ants.image_read( targetfn )
+img1 = ants.image_read( targetfn )
+img2 = ants.image_read( targetfn2 )
 print("begin: " + newprefix )
-# some work on this modality here
-print("complete: " + newprefix )
+dwp = antspymm.dewarp_imageset( [img1,img2], iterations=2, padding=6,
+    syn_sampling = 20, syn_metric='mattes',
+    type_of_transform = 'SyN',
+    total_sigma = 0.0, random_seed=1,
+    reg_iterations = [200,50,20,0] )
 
-# write stuff out - this is just an example that needs to be changed
-# for a specific modality
-mtlfn = os.path.expanduser( "~/.antspyt1w/mtl_description.csv" )
-if not exists( mtlfn ):
-    t1h['medial_temporal_lobe'][ 'mtl_description'].to_csv( mtlfn )
-mtldf = antspyt1w.map_segmentation_to_dataframe(
-    'mtl_description', t1h['medial_temporal_lobe'][ 'mtl_segmentation' ] )
-ants.image_write( t1h['medial_temporal_lobe'][ 'mtl_segmentation' ],
-    newprefix + "mtl.nii.gz" )
-(mtldf).to_csv( newprefix + "mtl.csv" )
+# ants.image_write( dwp['dewarped'][0], newprefixList[0] + 'SRdewarped.nii.gz' )
+# ants.image_write( dwp['dewarped'][1], newprefixList[1] + 'SRdewarped.nii.gz' )
+
+# now reconstruct DTI
+print("Begin Recon 1")
+bvec = re.sub( "-SR.nii.gz", ".bvec", targetfn )
+bval = re.sub( "-SR.nii.gz", ".bval", targetfn )
+dd = antspymm.dipy_dti_recon( dwp['dewarped'][0], bval, bvec )
+for mykey in ['MD','FA','RGB']:
+    ants.image_write( dd[mykey],  newprefixList[0] + 'SR' + mykey, '.nii.gz' )
+
+print("Begin Recon 2")
+bvec = re.sub( "-SR.nii.gz", ".bvec", targetfn2 )
+bval = re.sub( "-SR.nii.gz", ".bval", targetfn2 )
+dd = antspymm.dipy_dti_recon( dwp['dewarped'][1], bval, bvec )
+for mykey in ['MD','FA','RGB']:
+    ants.image_write( dd[mykey],  newprefixList[1] + 'SR' + mykey, '.nii.gz' )
+print("complete: " + newprefixList[0] + " & " + newprefixList[2] )
