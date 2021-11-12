d529f7bbf6ccfa867e43c8b31ba19759b6ef77df
stnava <stnava@gmail.com>
2021-11-12 10:06:08 -0500

    WIP: attempt to use Rmd

--- src/rsfmri/01_job_id_subscript.sh
+++ src/rsfmri/01_job_id_subscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo TASK ID is $SLURM_ARRAY_TASK_ID
-python3 ~/coderepo/niall/src/rsfmri/02_job_script_dfn.py $SLURM_ARRAY_TASK_ID
+Rscript ~/coderepo/niall/src/rsfmri/02_job_script_dfn.R $SLURM_ARRAY_TASK_ID
---
+++ src/rsfmri/02_job_script_dfn.R
@@ -0,0 +1,1 @@
+rmarkdown::render("~/code/niall/src/rsfmri/03_dfn.Rmd")
