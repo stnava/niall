ca6a65e6c464819160f525eb1116037828e657c9
stnava <stnava@gmail.com>
2021-11-12 10:14:00 -0500

    BUG: need pandoc

--- src/rsfmri/02_job_script_dfn.R
+++ src/rsfmri/02_job_script_dfn.R
@@ -1,1 +1,4 @@
-rmarkdown::render("~/code/niall/src/rsfmri/03_dfn.Rmd")
+system("sudo apt-get install pandoc")
+outfn=tempfile( fileext='.html' )
+print(outfn)
+rmarkdown::render("~/code/niall/src/rsfmri/03_dfn.Rmd",output_file=outfn)
