8e19b0647f1bfaa801e43953901db6541c3dc075
stnava <stnava@gmail.com>
2021-11-14 22:19:02 +0000

    BUG: ...

--- src/nmsr/nmsr.py
+++ src/nmsr/nmsr.py
@@ -1,7 +1,7 @@
 import os
 import glob
 modality = "Neuromelanin"
-dtifns = glob.glob( "/mnt/cluster/data/PPMI2/PPMI/*/*/Neuromelanin/*/*/*/*gz" )
+dtifns = glob.glob( "/mnt/cluster/data/PPMI2/PPMI/*/*/Neuromelanin/*/*/*/*V0.nii.gz" )
 import sys
 # 735 of these ! print( len( dtifns ) )
 if len( sys.argv ) > 1:
@@ -11,25 +11,29 @@ else:
 
 dtifn = dtifns[ fileindex ]
 import re
-outfn = re.sub('dcm2niix-V0.nii.gz', 'SSRS.nii.gz', dtifn )
-from os.path import exists
-myoutfnexists = exists( outfn )
+outfn = re.sub('dcm2niix-V0.nii.gz', 'SSRS.nii.gz', str( dtifn ) )
+outfn2 = re.sub('dcm2niix-V0.nii.gz', 'brainSR.nii.gz', str( dtifn ) )
 
+from os.path import exists
+myoutfnexists = ( exists( outfn ) and exists(outfn2) )
 if myoutfnexists:
     print( outfn + "exists already" )
-    sys.exit()
 else:
     print( "BeginNMSR " + outfn )
 import ants
 import antspymm
 import tensorflow as tf
 import superiq
-
+import antspynet
 mdlfn = "/home/ubuntu/models/SEGSR_32_ANINN222_3.h5"
-# mdl = tf.keras.models.load_model( mdlfn )
-dti = ants.image_read( dtifn ).denoise_image( noise_model='Gaussian')
-hemi = ants.get_mask(img).iMath("ME",2).iMath("GetLargestComponent")
+dti = ants.image_read( dtifn ).denoise_image( noise_model='Gaussian',p="2x2x1",r="3x3x1").iMath("TruncateIntensity",0.01,0.995)
+hemi = ants.get_mask(dti).iMath("ME",2).iMath("GetLargestComponent")
 mysr = superiq.super_resolution_segmentation_per_label( dti, hemi, [2,2,2], mdlfn, [1], dilation_amount=0, probability_images=None, probability_labels=None, max_lab_plus_one=True, verbose=True )
 
 ants.image_write( mysr['super_resolution'], outfn )
+
+mdlfn = antspymm.get_data( "brainSR", target_extension=".h5")
+mdl = tf.keras.models.load_model( mdlfn )
+mysr2 = antspynet.apply_super_resolution_model_to_image( dti, mdl )
+ants.image_write( mysr2, outfn2 )
 
