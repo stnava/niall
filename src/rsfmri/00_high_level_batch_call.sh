b170313878339ca09faa6a1d78697ba692523024
stnava <stnava@gmail.com>
2021-11-15 12:24:41 +0000

    ENH: bf snc

--- src/rsfmri/00_high_level_batch_call.sh
+++ src/rsfmri/00_high_level_batch_call.sh
@@ -1,2 +1,2 @@
-sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/rsfmriSR.%a.out  \
+sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/rsfmri%a.out  \
   --array=0-281  ~/coderepo/niall/src/rsfmri/01_job_id_subscript.sh
--- src/rsfmri/01_job_id_subscript.sh
+++ src/rsfmri/01_job_id_subscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo TASK ID is $SLURM_ARRAY_TASK_ID
-# python3 ~/coderepo/niall/src/rsfmri/02_job_script.py $SLURM_ARRAY_TASK_ID
-Rscript ~/coderepo/niall/src/rsfmri/02_job_script_dfn.R $SLURM_ARRAY_TASK_ID
+python3 ~/coderepo/niall/src/rsfmri/02_job_script_snbf.py $SLURM_ARRAY_TASK_ID
+# Rscript ~/coderepo/niall/src/rsfmri/02_job_script_dfn.R $SLURM_ARRAY_TASK_ID
