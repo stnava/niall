3379b95cafd6dd70abd1781e82ec684ca356b179
stnava <stnava@gmail.com>
2022-01-01 14:09:40 -0500

    BUG: forgot antspynet

--- src/t1/02_job_script.py
+++ src/t1/02_job_script.py
@@ -12,6 +12,7 @@ if not exists( rootdir ):
 
 t1fns = glob.glob( rootdir + "PPMI2/PPMI/*/*/T1w/*/dcm2niix/V0/*-dcm2niix-V0.nii.gz" )
 t1fns = t1fns + glob.glob( rootdir + "PPMI1/*/*/*/*/*nii.gz" )
+
 import sys
 fileindex = 0
 dosr = True
@@ -50,6 +51,7 @@ if myoutfnexists:
 
 print( "continue " +  outfn + " run " )
 import ants
+import antspynet
 import antspymm
 import tensorflow as tf
 import antspyt1w
@@ -68,19 +70,17 @@ if dosr:
     templatea = ants.image_read( tfn )
     templatea = ( templatea * antspynet.brain_extraction( templatea, 't1' ) ).iMath( "Normalize" )
     templatealr = ants.image_read( tlrfn )
-    mylr = label_hemispheres( img, templatea, templatealr )
-    derk
+    mylr = antspyt1w.label_hemispheres( t1 * t1bxt, templatea, templatealr )
     print("second is SR")
     mdlfn = "/home/ubuntu/models/SEGSR_32_ANINN222_3.h5"
     mdl = tf.keras.models.load_model( mdlfn )
     mysr = superiq.super_resolution_segmentation_per_label(
-        t1, t1bxt, [2,2,2], mdl, [1], dilation_amount=6, probability_images=None,
+        t1, mylr, [2,2,2], mdl, [1,2], dilation_amount=0, probability_images=None,
         probability_labels=None, max_lab_plus_one=False, verbose=True )
     t1 = mysr['super_resolution']
     t1bxt = ants.resample_image_to_target( t1bxt, t1, interp_type='nearestNeighbor' )
     ants.image_write( t1, newprefix + ".nii.gz" )
     print("begin hier: " + newprefix )
-    derka
     t1h = antspyt1w.hierarchical( t1, output_prefix=newprefix, imgbxt=t1bxt, cit168=True )
 else:
     print("begin hier: " + newprefix )
