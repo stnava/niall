357a620a1cdccbf01857b11fa8bb6361b01f2824
stnava <stnava@gmail.com>
2022-01-01 21:15:08 +0000

    PERF: to get around 1000 job limit

--- src/t1/00_high_level_batch_call.sh
+++ src/t1/00_high_level_batch_call.sh
@@ -1,2 +1,5 @@
-sbatch  --export=ALL --cpus-per-task 24  -o ~/slurmout/t1.%a.out  \
-  --array=0-143  ~/coderepo/niall/src/t1/01_job_id_subscript.sh
+export SLURM_CONF=/opt/slurm/etc/slurm.conf
+for offset in 0 1000 2000 ; do
+  sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/t1.%a.out  \
+    --array=0-1000 ./01_job_id_subscript.sh $offset
+done
--- src/t1/01_job_id_subscript.sh
+++ src/t1/01_job_id_subscript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo TASK ID is $SLURM_ARRAY_TASK_ID
-python3 ~/coderepo/niall/src/t1/02_job_script.py $SLURM_ARRAY_TASK_ID
+echo $1
+python3 ~/coderepo/niall/src/t1/02_job_script.py $SLURM_ARRAY_TASK_ID True $1
