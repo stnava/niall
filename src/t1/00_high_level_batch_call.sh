480df3d187da6bae8a30ef60c85fe786296015f6
stnava <stnava@gmail.com>
2022-01-03 21:26:02 +0000

    ENH: >>>

--- src/t1/00_high_level_batch_call.sh
+++ src/t1/00_high_level_batch_call.sh
@@ -1,5 +1,5 @@
 export SLURM_CONF=/opt/slurm/etc/slurm.conf
 for offset in 0 1000 2000 ; do
-  sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/t1.%a.out  \
+  sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/PPMI_${offset}x.%a.out  \
     --array=0-1000 ./01_job_id_subscript.sh $offset
 done
