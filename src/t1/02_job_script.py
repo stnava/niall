ba524879eb9b62f7d58c092d39031adc6ecdbf90
stnava <stnava@gmail.com>
2022-01-05 19:08:11 -0500

    ENH: better ppmi approach

--- src/t1/02_job_script.py
+++ src/t1/02_job_script.py
@@ -50,17 +50,9 @@ if not myx:
     os.makedirs( newoutdir, exist_ok=True  )
 
 print( "made " +  newoutdir + " successfully " )
-if not dosr:
-    outfn = newprefix + "hippLR" + '.nii.gz'
-else:
-    outfn = newprefix + "-SR" + "hippLR" + '.nii.gz'
 
-myoutfnexists = exists( outfn )
-if myoutfnexists:
-    print( outfn + "exists already" )
-    sys.exit()
 
-print( "continue " +  outfn + " run " )
+print( "RUN " +  newprefix  + " " )
 import ants
 import antspynet
 import antspymm
@@ -69,73 +61,44 @@ import tensorflow.keras.backend as K
 K.set_floatx("float32")
 import antspyt1w
 import superiq
-t1 = ants.image_read( t1fn )
-if dosr:
-    print("begin: " + newprefix +  " dosr " + str( dosr ) )
-    print("first a bxt ")
-    t1 = ants.iMath( t1, "TruncateIntensity", 1e-4, 0.999 ).iMath( "Normalize" )
-    t1bxt = antspyt1w.brain_extraction( t1 )
-    t1 = ants.denoise_image( t1, t1bxt, noise_model='Gaussian')
-    t1 = ants.n4_bias_field_correction( t1, mask=t1bxt, rescale_intensities=True, ).iMath("Normalize")
-    tfn = antspyt1w.get_data('T_template0', target_extension='.nii.gz' )
-    tlrfn = antspyt1w.get_data('T_template0_LR', target_extension='.nii.gz' )
-    templatea = ants.image_read( tfn )
-    templatea = ( templatea * antspynet.brain_extraction( templatea, 't1' ) ).iMath( "Normalize" )
-    templatealr = ants.image_read( tlrfn )
-    t1crop = ants.crop_image( t1 * t1bxt, ants.iMath(  t1bxt, "MD", 6 ) )
-    t1crop = ants.iMath( t1crop, "TruncateIntensity", 1e-4, 0.999 ).iMath( "Normalize" )
-    ants.image_write( t1crop, newprefix + "-brain_n4_dnz.nii.gz" )
-    newprefix = newprefix + "-SR"
-    print( "t1crop" )
-    print( t1crop )
-    mylr = antspyt1w.label_hemispheres( t1crop, templatea, templatealr )
-    print("second is SR")
-    mdlfn = "/home/ubuntu/models/SEGSR_32_ANINN222_3.h5"
-    mdl = tf.keras.models.load_model( mdlfn )
-    mysr = superiq.super_resolution_segmentation_per_label(
-        t1crop, mylr, [2,2,2], mdl, [1,2], dilation_amount=0, probability_images=None,
-        probability_labels=None, max_lab_plus_one=False, verbose=True )
-    t1 = mysr['super_resolution']
-    t1bxt = ants.resample_image_to_target( t1bxt, t1, interp_type='nearestNeighbor' )
-    ants.image_write( t1, newprefix + ".nii.gz" )
-    print("begin hier: " + newprefix )
-    t1h = antspyt1w.hierarchical( t1, output_prefix=newprefix, imgbxt=t1bxt, cit168=True )
-else:
-    print("begin hier: " + newprefix )
-    t1h = antspyt1w.hierarchical( t1, output_prefix=newprefix, cit168=True )
-
-print("complete: " + newprefix )
-
-# write extant dataframes
-for myvar in t1h['dataframes'].keys():
-    if t1h['dataframes'][myvar] is not None:
-        t1h['dataframes'][myvar].dropna(0).to_csv(newprefix + myvar + ".csv")
-
-(t1h['rbp']).to_csv( newprefix + "rbp.csv" )
-
-myvarlist = [
-    'brain_n4_dnz',
-    'brain_extraction',
-    'wm_tractsL',
-    'wm_tractsR',
-    'bf',
-    'mtl',
-    'snseg',
-    'deep_cit168lab',
-    'cit168lab',
-    'left_right' ]
-myvarlist = t1h.keys()
-r16img = ants.image_read( ants.get_data( "r16" ))
-for myvar in myvarlist:
-    if t1h[myvar] is not None and type(t1h[myvar]) == type( r16img ):
-        ants.image_write( t1h[myvar], newprefix + myvar + '.nii.gz' )
-
-myvarlist = [
-    'tissue_segmentation',
-    'dkt_parcellation',
-    'dkt_lobes',
-    'dkt_cortex',
-    'hemisphere_labels' ]
-for myvar in myvarlist:
-    if t1h['dkt_parc'][myvar] is not None:
-        ants.image_write( t1h['dkt_parc'][myvar], newprefix + myvar + '.nii.gz' )
+tfn = antspyt1w.get_data('T_template0', target_extension='.nii.gz' )
+tlrfn = antspyt1w.get_data('T_template0_LR', target_extension='.nii.gz' )
+templatea = ants.image_read( tfn )
+templatea = ( templatea * antspynet.brain_extraction( templatea, 't1' ) ).iMath( "Normalize" )
+templatealr = ants.image_read( tlrfn )
+bxtsylelist = ['v1']
+for bxtstyle in bxtsylelist:
+    srfnout = newprefix + "_" + bxtstyle
+    if not exists( srfnout + "SR_mergewide.csv" ):
+        print("begin: " + srfnout  )
+        t1 = ants.image_read( t1fn )
+        t1bxt = antspyt1w.brain_extraction( t1, method=bxtstyle, verbose=True )
+        t1 = antspyt1w.preprocess_intensity( t1, t1bxt )
+        if bxtstyle == "v3":
+            t1 = ants.rank_intensity( t1 )
+        t1crop = ants.crop_image( t1, ants.iMath(  t1bxt, "MD", 6 ) )
+        ants.image_write( t1crop, srfnout + "brain_n4_dnz.nii.gz" )
+        print( "t1crop" )
+        print( t1crop )
+        mylr = antspyt1w.label_hemispheres( t1crop, templatea, templatealr )
+        print("second is SR")
+        mdlfn = "/home/ubuntu/models/SEGSR_32_ANINN222_3.h5"
+        mdl = tf.keras.models.load_model( mdlfn )
+        mysr = superiq.super_resolution_segmentation_per_label(
+                t1crop, mylr, [2,2,2], mdl, [1,2], dilation_amount=0, probability_images=None,
+                probability_labels=None, max_lab_plus_one=False, verbose=True )
+        t1 = mysr['super_resolution']
+        t1bxt = ants.resample_image_to_target( t1bxt, t1, interp_type='nearestNeighbor' )
+        srfnout = srfnout + "SR"
+        ants.image_write( t1, srfnout + ".nii.gz" )
+        ants.image_write( t1bxt, srfnout + "brain_extraction.nii.gz" )
+        print("begin hier: " + srfnout )
+        t1h = antspyt1w.hierarchical( t1, output_prefix=srfnout, imgbxt=t1bxt, cit168=True )
+        antspyt1w.write_hierarchical( t1h, output_prefix=srfnout )
+        uid = os.path.basename(srfnout)
+        uid = re.sub(".nii.gz","",uid)
+        outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( t1h['dataframes'], identifier=uid )
+        outdf.to_csv( srfnout + "_mergewide.csv" )
+        print("complete: " + srfnout )
+    else:
+        print("already done: " + srfnout )
