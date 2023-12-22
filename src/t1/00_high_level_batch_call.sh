7ff0c49874d8623b1d32f72b42a6353f26547b4c
stnava <stnava@gmail.com>
2023-12-22 14:11:19 +0000

    ENH: for antspymm

--- src/t1/00_high_level_batch_call.sh
+++ src/t1/00_high_level_batch_call.sh
@@ -1,2 +1,2 @@
-sbatch  --export=ALL --cpus-per-task $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS  -o ~/slurmout/t1.%a.out  \
+sbatch  --export=ALL --cpus-per-task 12  -o ~/slurmout/t1.%a.out  \
   --array=0-143  ~/coderepo/niall/src/t1/01_job_id_subscript.sh
--- src/t1/02_job_script.py
+++ src/t1/02_job_script.py
@@ -2,95 +2,81 @@ import os
 from os.path import exists
 import glob
 
-rootdir = "/mnt/cluster/data/SRPBS_multidisorder_MRI/traveling_subjects/SRPBTravel/"
-t1fns = glob.glob( rootdir + "*/anat/*.nii.gz" )
+####################################################################################
+
+base_directory = "/mnt/cluster/data/SRPBS_multidisorder_MRI/"
+rootdir = base_directory + "traveling_subjects/SRPBTravel/"
+t1fns = glob.glob( rootdir + "sub-*" )
 t1fns.sort()
 import sys
 fileindex = 39
 if len( sys.argv ) > 1:
     fileindex = int(sys.argv[1])
+
+if fileindex > len( t1fns ):
+    sys.exit(0)
+
 t1fn = t1fns[ fileindex ]
 import re
-mysubbed = re.sub('anat', 'T1wH', t1fn )
-mysubbed = re.sub('traveling_subjects/SRPBTravel','traveling_subjects_repro_study',mysubbed)
-mysubbed = re.sub('.nii.gz','',mysubbed)
-newprefix = re.sub('_T1w','_T1wH',mysubbed)
-mysubbedsplit = newprefix.split("/")
-# define the directories and create them
-newoutdir = ''
-keyindex = len(mysubbedsplit) - 1 # change for each case
-for k in range(keyindex):
-    newoutdir = newoutdir + '/' + mysubbedsplit[k]
-newoutdir=newoutdir+'/'
+newoutdir = base_directory + 'traveling_subjects_antspymm/'
 os.makedirs( newoutdir, exist_ok=True  )
+os.makedirs( base_directory + 'studycsvs', exist_ok=True  )
 
-print( "RUN " +  newprefix  + " " )
-import ants
-import antspynet
+subject_id = os.path.basename( t1fn )
+
+print( "RUN " + subject_id + " --- " + newoutdir + " " )
 import antspymm
-import tensorflow as tf
-import tensorflow.keras.backend as K
-K.set_floatx("float32")
-import antspyt1w
-import superiq
-tfn = antspyt1w.get_data('T_template0', target_extension='.nii.gz' )
-tlrfn = antspyt1w.get_data('T_template0_LR', target_extension='.nii.gz' )
-templatea = ants.image_read( tfn )
-templatea = ( templatea * antspynet.brain_extraction( templatea, 't1' ) ).iMath( "Normalize" )
-templatealr = ants.image_read( tlrfn )
-bxtsylelist = ['v0','v1','v2','v3']
-bxtsylelist = ['v1','v3']
-for bxtstyle in bxtsylelist:
-    srfnout = newprefix + "_" + bxtstyle
-    print( srfnout + "SR_mergewide.csv" )
-    t1 = ants.image_read( t1fn ).iMath("Normalize")
-    bxt = antspyt1w.brain_extraction( t1, method=bxtstyle, verbose=True )
-    clusoe = antspyt1w.resnet_grader( t1 * bxt )
-    outdf.to_csv( srfnout + "inspection.csv" )
-    if not exists( srfnout + "SR_mergewide.csv" ) or not exists( srfnout + "OR_mergewide.csv" ):
-        print("begin: " + srfnout  )
-        t1 = ants.image_read( t1fn ).iMath("Normalize")
-        t1bxt = antspyt1w.brain_extraction( t1, method=bxtstyle, verbose=True )
-        t1 = antspyt1w.preprocess_intensity( t1, t1bxt )
-        if bxtstyle == "v3":
-            t1 = ants.rank_intensity( t1 )
-        t1crop = ants.crop_image( t1, ants.iMath(  t1bxt, "MD", 6 ) )
-        t1bxt = ants.crop_image( t1bxt.clone(), ants.iMath(  t1bxt, "MD", 6 ) )
-        ants.image_write( t1crop, srfnout + "brain_n4_dnz.nii.gz" )
-        print( "t1crop" )
-        print( t1crop )
-        if bxtstyle == 'v1' and not exists( srfnout + "OR_mergewide.csv" ) :
-            orfnout = srfnout + "OR"
-            print("begin hier: " + orfnout )
-            t1h = antspyt1w.hierarchical( t1crop, output_prefix=orfnout, imgbxt=t1bxt, cit168=True )
-            antspyt1w.write_hierarchical( t1h, output_prefix=orfnout)
-            uid = os.path.basename(orfnout)
-            uid = re.sub(".nii.gz","",uid)
-            outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( t1h['dataframes'], identifier=uid )
-            outdf.to_csv( orfnout + "_mergewide.csv" )
-            print("complete: " + orfnout )
-        if not exists( srfnout + "SR_mergewide.csv" ) :
-            mylr = antspyt1w.label_hemispheres( t1crop, templatea, templatealr )
-            mylr = antspyt1w.subdivide_hemi_label( mylr )
-            print("second is SR")
-            mdlfn = "/home/ubuntu/models/SEGSR_32_ANINN222_3.h5"
-            mdl = tf.keras.models.load_model( mdlfn )
-            mysr = superiq.super_resolution_segmentation_per_label(
-                t1crop, mylr, [2,2,2], mdl, [4,5,6,7], dilation_amount=0, probability_images=None,
-                probability_labels=None, max_lab_plus_one=False, verbose=True )
-            t1 = mysr['super_resolution']
-            t1bxt = ants.resample_image_to_target( t1bxt, t1, interp_type='nearestNeighbor' )
-            srfnout = srfnout + "SR"
-            ants.image_write( t1, srfnout + ".nii.gz" )
-            ants.image_write( t1bxt, srfnout + "brain_extraction.nii.gz" )
-            print("begin hier: " + srfnout )
-            t1h = antspyt1w.hierarchical( t1, output_prefix=srfnout, imgbxt=t1bxt, cit168=True )
-            antspyt1w.write_hierarchical( t1h, output_prefix=srfnout )
-            uid = os.path.basename(srfnout)
-            uid = re.sub(".nii.gz","",uid)
-            outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( t1h['dataframes'], identifier=uid )
-            outdf.to_csv( srfnout + "_mergewide.csv" )
-            print("complete: " + srfnout )
-    else:
-        print("already done: " + srfnout + " andORTOO" )
+
+import os
+nth='12'
+os.environ["TF_NUM_INTEROP_THREADS"] = nth
+os.environ["TF_NUM_INTRAOP_THREADS"] = nth
+os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = nth
+import sys
+import pandas as pd
+import glob
+import ants
+
+import antspymm
+from antspymm import generate_mm_dataframe
+
+
+template = ants.image_read("~/.antspymm/PPMI_template0.nii.gz")
+bxt = ants.image_read("~/.antspymm/PPMI_template0_brainmask.nii.gz")
+template = template * bxt
+template = ants.crop_image( template, ants.iMath( bxt, "MD", 12 ) )
+
+anatfn = t1fn + '/anat/' + subject_id + "_T1w.nii.gz"
+rsfn = t1fn + '/func/' + subject_id + '_task-rest_run-01_bold.nii.gz'
+if not os.path.exists( anatfn ):
+    print( anatfn + " does not exist : exiting ")
+    sys.exit(0)
+
+# generate_mm_dataframe(projectID, subjectID, date, imageUniqueID, modality, source_image_directory, output_image_directory, t1_filename, flair_filename=[], rsf_filenames=[], dti_filenames=[], nm_filenames=[], perf_filename=[])
+
+studycsv = antspymm.generate_mm_dataframe(
+        'SRPBS',
+        subject_id,
+        'ses-1',
+        '000',
+        'T1w',
+        rootdir,
+        newoutdir,
+        t1_filename = anatfn,
+        rsf_filenames = [rsfn],
+    )
+studycsv.to_csv(base_directory + "studycsvs/" + subject_id + ".csv")
+studycsv2 = studycsv.dropna(axis=1)
+mmrun = antspymm.mm_csv(studycsv2,
+                        dti_motion_correct='SyN',
+                        dti_denoise=True,
+                        normalization_template=template,
+                        normalization_template_output='ppmi',
+                        normalization_template_transform_type='antsRegistrationSyNQuickRepro[s]',
+                        normalization_template_spacing=[1,1,1],
+                        mysep='_')  # should be this
+
+
+
+
 
